4cb051123d45c3bd9b2ed4ffd166e21a8222e89b
Nozaaaaa <giorginozadze012@gmail.com>
2024-06-08 03:18:07 -0700

    second commit

---
+++ day 05/classwork.py
@@ -0,0 +1,14 @@
+
+password = input("Please enter new password: ")
+re_password = input("Please re-enter password: ")
+
+print(password == re_password)
+
+num1 =input("enter your name")
+num2 =input("enter second name")
+
+print(num1 > num2)
+print(num1 < num2)
+print(num1 <= num2)
+print(num1 >= num2)
+print(num1 == num2)
---
+++ day 05/homework2.py
@@ -0,0 +1,18 @@
+
+num1 =5
+num2 =10
+print("num1" < "num2")
+print("num1" > "num2")
+print("num1" <= "num2")
+print("num1" >= "num2")
+print("num1" == "num2")
+
+
+
+
+
+
+
+
+
+
