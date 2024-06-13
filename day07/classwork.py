8f34887ae661ef7d87701b6fde58e0519d39584b
Nozaaaaa <giorginozadze012@gmail.com>
2024-06-13 02:54:26 -0700

    commit

---
+++ day07/classwork.py
@@ -0,0 +1,14 @@
+for i in range(10):
+   print(str(i) + "." + "batono davit mamapapuri kaci canxart")
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day07/homework.py
@@ -0,0 +1,36 @@
+for i in range(10):
+   print(str(i) + "." + "goa is the best")
+
+for i in range(10):
+   print(str(i) + "." + "giorgi nozadze")
+
+for i in range(10):
+   print(str(i) + "." + "i love you")
+
+for i in range(10):
+   print(str(i) + "." + "kargi gakvetilebi")
+
+for i in range(10):
+   print(str(i) + "." + "kargi mentorebi")
+
+for i in range(10):
+   print(str(i) + "." + "luka cxvaradze")
+
+for i in range(10):
+   print(str(i) + "." + "kargia goashi swavla")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
